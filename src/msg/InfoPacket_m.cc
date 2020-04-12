7aeb4e9847e41437f88574f5aa32d46b9f4cc483
A. G. Medrano-Chávez <ryuuba@gmail.com>
2020-04-12 15:39:02 -0500

    Update INET framework

--- src/msg/InfoPacket_m.cc
+++ src/msg/InfoPacket_m.cc
@@ -1,5 +1,5 @@
 //
-// Generated file, do not edit! Created by nedtool 5.6 from base/../base/../contract/../msg/InfoPacket.msg.
+// Generated file, do not edit! Created by nedtool 5.6 from msg/InfoPacket.msg.
 //
 
 // Disable warnings about unused variables, empty switch stmts, etc:
